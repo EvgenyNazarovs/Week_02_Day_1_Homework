229bb1be6a35a57195065fff2d511f37f424873a
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-03-16 20:49:05 +0000

    adds both setter methods

---
+++ codeclan_student.rb
@@ -0,0 +1,24 @@
+class Student
+
+  def initialize(student_name, cohort)
+    @student_name = student_name
+    @cohort = cohort
+  end
+
+  def student_name
+    return @student_name
+  end
+
+  def cohort
+    return @cohort
+  end
+
+  def set_student_name(student_name)
+    @student_name = student_name
+  end
+
+  def set_cohort(cohort)
+    @cohort = cohort
+  end 
+
+end
---
+++ specs/test_codeclan_student.rb
@@ -0,0 +1,58 @@
+require('minitest/autorun')
+require('minitest/reporters')
+
+require_relative('../codeclan_student.rb')
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+class TestCodeclanStudent < Minitest::Test
+
+  def setup
+    @student = Student.new("Evgeny", "G18")
+  end
+
+  def test_student_name
+    assert_equal("Evgeny", @student.student_name)
+  end
+
+  def test_student_cohort
+    assert_equal("G18", @student.cohort)
+  end
+
+  def test_set_student_name
+    assert_equal("Alex", @student.set_student_name("Alex"))
+  end
+
+  def test_set_cohort
+    assert_equal("G19", @student.set_cohort("G19"))
+  end 
+
+
+
+end
+
+
+
+
+
+
+
+=begin
+Part A
+For this part we want you to make a class that represents a CodeClan student.
+
+Create a class called Student that takes in a name (String) and a cohort
+(string - e.g “E18”, “G6”, etc) when an new instance is created.
+
+Create a couple of Getter methods, one that returns the name property and
+one that returns the cohort property of the student.
+
+
+Add in Setter methods to update the students name and what cohort they are in.
+
+
+Create a method that gets the student to talk (eg. Returns “I can talk!).
+Create a method that takes in a students favourite programming language and
+returns it as part of a string
+(eg. student1.say_favourite_language("Ruby") -> “I love Ruby”).
+=end
